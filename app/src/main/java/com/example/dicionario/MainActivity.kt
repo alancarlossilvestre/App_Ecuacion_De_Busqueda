638976f27265a689ea76cbf0ba0f5816f2440b03
Alan Carlos <alancarlossilvestre1@gmail.com>
2023-05-28 23:28:38 -0500

    funcionalidad de agregar palabras lista

--- app/src/main/java/com/example/dicionario/MainActivity.kt
+++ app/src/main/java/com/example/dicionario/MainActivity.kt
@@ -1,45 +1,66 @@
 package com.example.dicionario
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import com.example.dicionario.controladorInput.InputManager
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textInputEditTextManager: textInputManager
+
     private lateinit var copiarTextoManager: copiarTextoManager
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var btnAgregar: Button
+    private lateinit var txtInputPalabra: TextInputEditText
+    private lateinit var txtInputSinonimo : TextInputEditText
 
-    private lateinit var textInputContainer: ViewGroup
-    private lateinit var textoAcumuladoTextView: TextView
-
+    private lateinit var btnAdd : AppCompatImageView
+    private var contadorP = 0
+    private var contadorS = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textInputContainer = findViewById(R.id.textInputContainer)
-        textoAcumuladoTextView = findViewById(R.id.textoAcumuladoTextView)
+        linearLayout = findViewById(R.id.linearLayoutMain)
+        btnAgregar = findViewById(R.id.btnAgregarPalabramain)
 
-        textInputEditTextManager = textInputManager(this, textInputContainer)
-        copiarTextoManager = copiarTextoManager(textInputContainer, textoAcumuladoTextView)
+        btnAgregar.setOnClickListener {
+            agregarElemento()
+        }
+    }
 
+    fun generarEcuacion(view: View) {
 
     }
 
-    fun agregarTextInputEditText(view: View) {
-        textInputEditTextManager.agregarTextInputEditText()
+    private fun agregarElemento(){
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val nuevoElemento = inflater.inflate(R.layout.palabras_y_sinonimos, null)
+
+        //val txtViewIngresePalabra = nuevoElemento.findViewById<TextView>(R.id.textViewIngresePalabra)
+        val txtInputPalabra =  nuevoElemento.findViewById<TextInputEditText>(R.id.palabra_1)
+        //val linearLayoutSinonimo = nuevoElemento.findViewById<LinearLayout>(R.id.textInputSinonimoContainer)
+        //val txtViewSP = nuevoElemento.findViewById<TextView>(R.id.txtViewPS)
+        val txtInputSinonimo =  nuevoElemento.findViewById<TextInputEditText>(R.id.inputEditTextSinonimo)
+        //val linearLayoutAddDelete = nuevoElemento.findViewById<LinearLayout>(R.id.layoutAddDelet)
+        btnAdd =  nuevoElemento.findViewById(R.id.addS)
+        //val btnDelete = nuevoElemento.findViewById<ImageView>(R.id.quitarS)
+
+        contadorP++
+        txtInputPalabra.hint = "Palabra $contadorP"
+        txtInputSinonimo.hint = "Sinonimo"
+
+        linearLayout.addView(nuevoElemento)
     }
-
-    fun copiarTexto(view: View) {
-        copiarTextoManager.copiarTexto()
-    }
-
-
 
 }
